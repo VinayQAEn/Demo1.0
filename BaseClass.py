0d0d393b21f2b2f21285f84e8063b7efce070c5e
Vinay <vinaybakoria99@gmail.com>
2024-09-05 19:27:16 +0530

    Newly commit

--- BaseClass.py
+++ BaseClass.py
@@ -11,15 +11,3 @@ class Baseclass:
         logger.setLevel(logging.DEBUG)
         return logger
 
-import inspect
-import logging
-class Baseclass:
-    def getlogger(self):
-        logger_name = inspect.stack()[1][3]
-        logger = logging.getLogger(logger_name)
-        formatter = logging.Formatter("%(asctime)s : %(name)s : %(levelname)s : %(message)s")
-        Filehandler = logging.FileHandler("logfile.log")
-        Filehandler.setFormatter(formatter)
-        logger.addHandler(Filehandler)
-        logger.setLevel(logging.DEBUG)
-        return logger
